ffe292d1b40b72c1ed64d75089cddb8495bc6872
kimdaye77 <kimdaye77@naver.com>
2021-05-27 23:42:14 +0900

    first commit

---
+++ chap2/Profile.java
@@ -0,0 +1,8 @@
+public class Profile {
+	public static void main(String[] args) {
+		String name="김다예";
+		int age=21;
+		System.out.println("My name is "+name);
+		System.out.println("My age is "+age);
+	}
+}
